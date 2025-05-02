56059df6731c21d652995fcb42388552d9c1bc8c
shiinasama <1762709630@qq.com>
2025-05-02 14:10:00 +0800

    2401213357-get_data

--- train_gpt2.py
+++ train_gpt2.py
@@ -87,7 +87,7 @@ class GPT(nn.Module):
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
-    def forward(self, idx):
+    def forward(self, idx,targets=None):
         B, T = idx.size()
         assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
         pos = torch.arange(0, T, dtype=torch.long, device=idx.device)
@@ -99,6 +99,12 @@ class GPT(nn.Module):
             x = block(x)
         x = self.transformer.ln_f(x)
         logits = self.lm_head(x)
+
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+        return logits, loss
+
         return logits
 
     @classmethod
@@ -165,8 +171,21 @@ model = GPT(GPTConfig())
 model.eval()
 model.to(device)
 
-
 import tiktoken
+
+enc = tiktoken.get_encoding('gpt2')
+with open('input.txt', 'r') as f:
+    text = f.read()
+text = text[:1000]
+tokens = enc.encode(text)
+B, T = 4, 32
+buf = torch.tensor(tokens[:B * T + 1])
+x = buf[:-1].view(B, T)
+y = buf[1:].view(B, T)
+logits = model(x)
+
+print(logits.shape)
+
 
 enc = tiktoken.get_encoding('gpt2')
 tokens = enc.encode("Hello, I'm a language model,")
@@ -175,6 +194,8 @@ tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
 x = tokens.to(device)
 torch.manual_seed(42)
 torch.cuda.manual_seed(42)
+
+
 
 while x.size(1) < max_length:
     with torch.no_grad():
