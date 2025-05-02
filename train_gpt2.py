7e85d4b32204c10ddd08fa1b7ab87b31b8d00544
shiinasama <1762709630@qq.com>
2025-05-02 19:50:40 +0800

    2401213357-output_tokenspeed

--- train_gpt2.py
+++ train_gpt2.py
@@ -237,9 +237,10 @@ for i in range(50):
     print(f"step {i}, loss: {loss.item()}")
     torch.cuda.synchronize()
     t1 = time.time()
-    dt = (t1 - t0) * 1000
-    tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
-    print(f"step {i}, loss: {loss.item()}, dt: {dt:.2f}ms, tok/sec: {tokens_per_sec:.2f}")
+    dt = t1 - t0  
+    tokens_processed = train_loader.B * train_loader.T
+    tokens_per_sec = tokens_processed / dt
+    print(f"step {i:4d} | loss: {loss.item():.6f} | dt: {dt * 1000:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
 
 
 enc = tiktoken.get_encoding('gpt2')
