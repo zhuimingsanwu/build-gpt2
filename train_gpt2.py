bd68275db9e30413b70702a6714ba28f01bb8626
shiinasama <1762709630@qq.com>
2025-05-02 16:08:17 +0800

    2401213357-train_process

--- train_gpt2.py
+++ train_gpt2.py
@@ -180,11 +180,17 @@ text = text[:1000]
 tokens = enc.encode(text)
 B, T = 4, 32
 buf = torch.tensor(tokens[:B * T + 1])
+buf = buf.to(device)
 x = buf[:-1].view(B, T)
 y = buf[1:].view(B, T)
-logits = model(x)
 
-print(logits.shape)
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+for i in range(50):
+    optimizer.zero_grad()
+    logits, loss = model(x, y)
+    loss.backward()
+    optimizer.step()
+    print(f"step {i}, loss: {loss.item()}")
 
 
 enc = tiktoken.get_encoding('gpt2')
