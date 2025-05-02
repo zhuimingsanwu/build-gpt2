31a43c67406593e64928b74f1a504057d35ee689
shiinasama <1762709630@qq.com>
2025-05-02 13:49:29 +0800

    2401213357-device_detect

--- train_gpt2.py
+++ train_gpt2.py
@@ -148,13 +148,22 @@ class GPT(nn.Module):
         return model
 
 
+
+
+device = "cpu"
+if torch.cuda.is_available():
+    device = "cuda"
+elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+    device = "mps"
+print(f"using device: {device}")
+
 num_return_sequences = 3
 max_length = 256
 
-model = GPT.from_pretrained('gpt2')
+model = GPT(GPTConfig())
 
 model.eval()
-model.to('cuda')
+model.to(device)
 
 
 import tiktoken
@@ -163,7 +172,7 @@ enc = tiktoken.get_encoding('gpt2')
 tokens = enc.encode("Hello, I'm a language model,")
 tokens = torch.tensor(tokens, dtype=torch.long)
 tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
-x = tokens.to('cuda')
+x = tokens.to(device)
 torch.manual_seed(42)
 torch.cuda.manual_seed(42)
 
